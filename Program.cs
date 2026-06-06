ef28e79b41225b4cf2f647f6f4a5d3be75656e46
unknown <teshalegetayalew@gmail.com>
2026-06-06 13:15:50 +0300

    feat(null-safety): add nullable reference example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Tms core System Started");
+﻿string? studentRegion = null;
+
+if (studentRegion is not null)
+{
+    Console.WriteLine(studentRegion.ToUpper());
+}
+else
+{
+    Console.WriteLine("Region not provided");
+}
