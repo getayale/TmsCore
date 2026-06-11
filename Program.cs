fae63a6834a71f0d43290c0afd82ae498ba72b46
getayale <teshalegetayalew.com>
2026-06-11 10:34:02 +0300

    Initial commit - M1 Session1 base

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿void PrintGradeReport(IEnumerable<IGradable> assessments)
+{
+    Console.WriteLine("--- Grade Report ---");
+
+    foreach (var item in assessments)
+    {
+        Console.WriteLine(
+            $"{item.Title}: {item.CalculateGrade():F2}%");
+    }
+}
+
+IGradable[] cohortAssessments =
+[
+    new Quiz
+    {
+        Title = "C# Basics",
+        CorrectAnswers = 18,
+        TotalQuestions = 20
+    },
+
+    new LabAssignment
+    {
+        Title = "Registration API",
+        FunctionalityScore = 90m,
+        CodeQualityScore = 85m
+    }
+];
+
+PrintGradeReport(cohortAssessments);
